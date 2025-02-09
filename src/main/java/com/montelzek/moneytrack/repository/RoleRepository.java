781db51c75419fec405fd80dd29ce7109174c780
montelzek <patrykmatras378@gmail.com>
2025-02-09 18:56:40 +0100

    Added @Repository annotation

--- src/main/java/com/montelzek/moneytrack/repository/RoleRepository.java
+++ src/main/java/com/montelzek/moneytrack/repository/RoleRepository.java
@@ -2,9 +2,11 @@ package com.montelzek.moneytrack.repository;
 
 import com.montelzek.moneytrack.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
     Optional<Role> findByName(Role.ERole name);
--- src/main/java/com/montelzek/moneytrack/repository/UserRepository.java
+++ src/main/java/com/montelzek/moneytrack/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.montelzek.moneytrack.repository;
 
 import com.montelzek.moneytrack.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
