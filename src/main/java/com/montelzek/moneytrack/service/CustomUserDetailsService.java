c89481a1e36e292c6f7c3e687403801f469266eb
montelzek <patrykmatras378@gmail.com>
2025-02-13 12:07:16 +0100

    Added @Transactional annotation to loadByUsername method to fix problem with lazy fetching roles

--- src/main/java/com/montelzek/moneytrack/service/CustomUserDetailsService.java
+++ src/main/java/com/montelzek/moneytrack/service/CustomUserDetailsService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.stream.Collectors;
 
@@ -20,6 +21,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
         User user = userRepository.findByEmail(email)
