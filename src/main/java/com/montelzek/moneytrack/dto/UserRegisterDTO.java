79af8c29b4a856249873228574c2ec948b3bcc1f
montelzek <patrykmatras378@gmail.com>
2025-02-19 13:48:16 +0100

    Add UserRegisterDTO for user registration

---
+++ src/main/java/com/montelzek/moneytrack/dto/UserRegisterDTO.java
@@ -0,0 +1,31 @@
+package com.montelzek.moneytrack.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRegisterDTO {
+
+    @NotBlank
+    @Email
+    @Size(max = 50)
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    @Size(max = 50)
+    private String firstName;
+
+    @Size(max = 50)
+    private String lastName;
+}
