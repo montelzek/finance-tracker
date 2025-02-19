93ad499f86599c2024f475c3c679b7376c67b4aa
montelzek <patrykmatras378@gmail.com>
2025-02-19 19:21:35 +0100

    Created Account entity

--- src/main/java/com/montelzek/moneytrack/model/Account.java
+++ src/main/java/com/montelzek/moneytrack/model/Account.java
@@ -1,4 +1,58 @@
 package com.montelzek.moneytrack.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "accounts")
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "account_type")
+    private AccountType accountType;
+
+    private Double balance;
+
+    @Enumerated(EnumType.STRING)
+    private Currency currency;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public enum AccountType {
+        SAVINGS,
+        CHECKING,
+        CREDIT_CARD,
+        CASH
+    }
+
+    public enum Currency {
+        USD,
+        EUR,
+        PLN
+    }
 }
