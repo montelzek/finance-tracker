c408906dd009b2b94243186fe335c59cb76d3932
montelzek <patrykmatras378@gmail.com>
2025-02-24 22:38:33 +0100

    Added Transaction entity

---
+++ src/main/java/com/montelzek/moneytrack/model/Transaction.java
@@ -0,0 +1,48 @@
+package com.montelzek.moneytrack.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @NotNull
+    private Double amount;
+
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    private LocalDate date;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
