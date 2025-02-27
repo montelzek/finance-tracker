e85c51c5ae389e3bb370770d78e578658b21a281
montelzek <patrykmatras378@gmail.com>
2025-02-27 21:31:21 +0100

    Added method to find categories by type

--- src/main/java/com/montelzek/moneytrack/repository/CategoryRepository.java
+++ src/main/java/com/montelzek/moneytrack/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.montelzek.moneytrack.repository;
 import com.montelzek.moneytrack.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    List<Category> findByType(String type);
 }
