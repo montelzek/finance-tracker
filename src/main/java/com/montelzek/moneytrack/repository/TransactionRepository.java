a6c650d5abc71f737ee8538888c68aeb6c64d8e1
montelzek <patrykmatras378@gmail.com>
2025-03-02 23:21:10 +0100

    Added @Repository annotation

--- src/main/java/com/montelzek/moneytrack/repository/TransactionRepository.java
+++ src/main/java/com/montelzek/moneytrack/repository/TransactionRepository.java
@@ -2,9 +2,11 @@ package com.montelzek.moneytrack.repository;
 
 import com.montelzek.moneytrack.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     List<Transaction> findByAccount_User_Id_OrderByCreatedAt(Long userId);
