98f85ffb70f0472dc6ac09ad2bf270f04ebebdf2
montelzek <patrykmatras378@gmail.com>
2025-03-02 23:28:54 +0100

    Implemented budget editing and deletion funcionality

--- src/main/java/com/montelzek/moneytrack/controller/BudgetController.java
+++ src/main/java/com/montelzek/moneytrack/controller/BudgetController.java
@@ -11,10 +11,7 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,16 +32,26 @@ public class BudgetController {
     @GetMapping
     public String listBudget(Model model) {
 
-        Long id = userService.getCurrentUserId();
-        List<Budget> budgetList = budgetService.findUsersBudgets(id);
-        List<Category> expenseCategories = categoryService.findByType("EXPENSE");
         BudgetDTO budgetDTO = new BudgetDTO();
-
         model.addAttribute("budget", budgetDTO);
-        model.addAttribute("budgets", budgetList);
-        model.addAttribute("expenseCategories", expenseCategories);
-
+        prepareBudgetModel(model);
         return "budgets/list";
+    }
+
+    @GetMapping("/edit/{id}")
+    @ResponseBody
+    public BudgetDTO getBudgetForEdit(@PathVariable Long id) {
+
+        Budget budget = budgetService.findById(id);
+
+        BudgetDTO budgetDTO = new BudgetDTO();
+        budgetDTO.setId(budget.getId());
+        budgetDTO.setName(budget.getName());
+        budgetDTO.setStartDate(budget.getStartDate());
+        budgetDTO.setEndDate(budget.getEndDate());
+        budgetDTO.setBudgetSize(budget.getBudgetSize());
+        budgetDTO.setCategoryId(budget.getCategory().getId());
+        return budgetDTO;
     }
 
     @PostMapping("/save")
@@ -52,7 +59,9 @@ public class BudgetController {
                              BindingResult result, Model model) {
 
         if (result.hasErrors()) {
-            return "accounts/list";
+            model.addAttribute("budget", budgetDTO);
+            prepareBudgetModel(model);
+            return "budgets/list";
         }
 
         Long userId = userService.getCurrentUserId();
@@ -60,18 +69,42 @@ public class BudgetController {
                 .orElseThrow(() -> new IllegalStateException("User not found"));
         Category category = categoryService.findById(budgetDTO.getCategoryId());
 
-        Budget budget = new Budget(
-                budgetDTO.getName(),
-                budgetDTO.getStartDate(),
-                budgetDTO.getEndDate(),
-                budgetDTO.getBudgetSize()
-        );
-        budget.setUser(user);
-        budget.setCategory(category);
-        budget.setBudgetSpent(0.0);
-
+        Budget budget;
+        if (budgetDTO.getId() != null) {
+            budget = budgetService.findById(budgetDTO.getId());
+            budget.setName(budgetDTO.getName());
+            budget.setStartDate(budgetDTO.getStartDate());
+            budget.setEndDate(budgetDTO.getEndDate());
+            budget.setBudgetSize(budgetDTO.getBudgetSize());
+            budget.setCategory(category);
+        } else {
+            budget = new Budget(
+                    budgetDTO.getName(),
+                    budgetDTO.getStartDate(),
+                    budgetDTO.getEndDate(),
+                    budgetDTO.getBudgetSize()
+            );
+            budget.setUser(user);
+            budget.setCategory(category);
+            budget.setBudgetSpent(0.0);
+        }
         budgetService.save(budget);
 
         return "redirect:/budgets";
+    }
+
+    @GetMapping("/delete")
+    public String deleteBudget(@RequestParam("budgetId") Long id) {
+        budgetService.deleteById(id);
+        return "redirect:/budgets";
+    }
+
+    private void prepareBudgetModel(Model model) {
+
+        Long id = userService.getCurrentUserId();
+        List<Budget> budgetList = budgetService.findUsersBudgets(id);
+        List<Category> expenseCategories = categoryService.findByType("EXPENSE");
+        model.addAttribute("budgets", budgetList);
+        model.addAttribute("expenseCategories", expenseCategories);
     }
 }
