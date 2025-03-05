91b83c61286cb5f83b3c909bad76d2bbb9bff5bb
montelzek <patrykmatras378@gmail.com>
2025-03-05 18:46:38 +0100

    Added methods to retrieve income and expense transactions from the past month

--- src/main/java/com/montelzek/moneytrack/repository/TransactionRepository.java
+++ src/main/java/com/montelzek/moneytrack/repository/TransactionRepository.java
@@ -2,6 +2,7 @@ package com.montelzek.moneytrack.repository;
 
 import com.montelzek.moneytrack.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,22 @@ import java.util.List;
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     List<Transaction> findByAccount_User_Id_OrderByCreatedAt(Long userId);
+
+    @Query("SELECT t FROM Transaction t " +
+            "JOIN t.account a " +
+            "JOIN t.category c " +
+            "JOIN a.user u " +
+            "WHERE c.type = 'INCOME' " +
+            "AND u.id = :userId " +
+            "AND t.date >= CURRENT_DATE - 30 DAY")
+    List<Transaction> findIncomeTransactionsFromPastMonth(Long userId);
+
+    @Query("SELECT t FROM Transaction t " +
+            "JOIN t.account a " +
+            "JOIN t.category c " +
+            "JOIN a.user u " +
+            "WHERE c.type = 'EXPENSE' " +
+            "AND u.id = :userId " +
+            "AND t.date >= CURRENT_DATE - 30 DAY")
+    List<Transaction> findExpenseTransactionsFromPastMonth(Long userId);
 }
