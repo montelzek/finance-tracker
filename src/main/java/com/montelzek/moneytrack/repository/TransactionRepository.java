5bce4d6c2878109480d1dcb0490af214e38deea6
montelzek <patrykmatras378@gmail.com>
2025-03-06 14:02:02 +0100

    Added query to find transactions from last 6 months

--- src/main/java/com/montelzek/moneytrack/repository/TransactionRepository.java
+++ src/main/java/com/montelzek/moneytrack/repository/TransactionRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -29,4 +30,11 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
             "AND u.id = :userId " +
             "AND t.date >= CURRENT_DATE - 30 DAY")
     List<Transaction> findExpenseTransactionsFromPastMonth(Long userId);
+
+    @Query("SELECT t FROM Transaction t " +
+            "JOIN t.account a " +
+            "JOIN a.user u " +
+            "WHERE u.id = :userId " +
+            "AND t.date >= :startDate")
+    List<Transaction> findTransactionsFromLastSixMonths(Long userId, LocalDate startDate);
 }
