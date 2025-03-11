b16ec032f3d80eedc0132bf1e27dd5fa853ef6d9
montelzek <patrykmatras378@gmail.com>
2025-03-11 17:34:20 +0100

    Added pagination support for finding transactions method

--- src/main/java/com/montelzek/moneytrack/repository/TransactionRepository.java
+++ src/main/java/com/montelzek/moneytrack/repository/TransactionRepository.java
@@ -1,6 +1,8 @@
 package com.montelzek.moneytrack.repository;
 
 import com.montelzek.moneytrack.model.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,7 +13,7 @@ import java.util.List;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    List<Transaction> findByAccount_User_Id_OrderByCreatedAt(Long userId);
+    Page<Transaction> findByAccount_User_Id_OrderByDateDesc(Long userId, Pageable pageable);
 
     List<Transaction> findTop6ByAccount_User_Id_OrderByDateDesc(Long userId);
 
