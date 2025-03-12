ba819ec1f6e3c4fe27442d255dc94ec40c80c244
montelzek <patrykmatras378@gmail.com>
2025-03-12 16:03:31 +0100

    Refactored update rates method to fetch only currencies rates I need

--- src/main/java/com/montelzek/moneytrack/service/ExchangeRateService.java
+++ src/main/java/com/montelzek/moneytrack/service/ExchangeRateService.java
@@ -2,13 +2,16 @@ package com.montelzek.moneytrack.service;
 
 import com.montelzek.moneytrack.util.ExchangeRateResponse;
 import jakarta.annotation.PostConstruct;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -20,7 +23,10 @@ public class ExchangeRateService {
     @Value("${currency.exchange.api.url}")
     private String apiUrl;
 
+    @Getter
     private Map<String, BigDecimal> rates = new HashMap<>();
+
+    private static final List<String> TARGET_CURRENCIES = Arrays.asList("EUR", "PLN", "GBP", "CHF", "JPY");
 
     private final RestTemplate restTemplate;
 
@@ -34,12 +40,13 @@ public class ExchangeRateService {
         String url = apiUrl + apiKey + "/latest/USD";
         try {
             ExchangeRateResponse response = restTemplate.getForObject(url, ExchangeRateResponse.class);
-            System.out.println("API Response: " + response);
             if (response != null && "success".equals(response.getResult())) {
                 Map<String, Double> doubleRates = response.getConversionRates();
                 rates.clear();
                 for (Map.Entry<String, Double> entry : doubleRates.entrySet()) {
-                    rates.put(entry.getKey(), new BigDecimal(entry.getValue().toString()));
+                    if (TARGET_CURRENCIES.contains(entry.getKey())) {
+                        rates.put(entry.getKey(), new BigDecimal(entry.getValue().toString()));
+                    }
                 }
             } else {
                 System.err.println("Failed to fetch rates: response is null, not successful, or conversion rates are null");
