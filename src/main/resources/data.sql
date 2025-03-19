9ba17c8dc71353d809a0606b81e3bd67f6860d56
montelzek <patrykmatras378@gmail.com>
2025-03-19 21:35:23 +0100

    Populated database with initial role and categories

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO roles (name) VALUES ('ROLE_USER');
+INSERT INTO roles (name) VALUES ('ROLE_PREMIUM');
+INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
+
+INSERT INTO categories (name, type) VALUES ('Salary', 'INCOME');
+INSERT INTO categories (name, type) VALUES ('Freelance Work', 'INCOME');
+INSERT INTO categories (name, type) VALUES ('Investment Returns', 'INCOME');
+INSERT INTO categories (name, type) VALUES ('Gifts', 'INCOME');
+INSERT INTO categories (name, type) VALUES ('Rental Income', 'INCOME');
+INSERT INTO categories (name, type) VALUES ('Groceries', 'EXPENSE');
+INSERT INTO categories (name, type) VALUES ('Rent', 'EXPENSE');
+INSERT INTO categories (name, type) VALUES ('Utilities', 'EXPENSE');
+INSERT INTO categories (name, type) VALUES ('Transportation', 'EXPENSE');
+INSERT INTO categories (name, type) VALUES ('Entertainment', 'EXPENSE');
+INSERT INTO categories (name, type) VALUES ('Dining Out', 'EXPENSE');
+INSERT INTO categories (name, type) VALUES ('Healthcare', 'EXPENSE');
+INSERT INTO categories (name, type) VALUES ('Insurance', 'EXPENSE');
+INSERT INTO categories (name, type) VALUES ('Shopping', 'EXPENSE');
+INSERT INTO categories (name, type) VALUES ('Financial Goal', 'FINANCIAL_GOAL');
