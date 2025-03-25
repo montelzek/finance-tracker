014e883d1cad043aa8673fb6b10eeb40084aad22
montelzek <patrykmatras378@gmail.com>
2025-03-25 19:31:10 +0100

    Removed unused imprts

--- src/test/java/com/montelzek/moneytrack/repository/RoleRepositoryTests.java
+++ src/test/java/com/montelzek/moneytrack/repository/RoleRepositoryTests.java
@@ -1,13 +1,11 @@
 package com.montelzek.moneytrack.repository;
 
 import com.montelzek.moneytrack.model.Role;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
