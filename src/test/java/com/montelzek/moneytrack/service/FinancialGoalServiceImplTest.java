38a5daa5b1197fd58a845f0c135932ea8bb25bd9
montelzek <patrykmatras378@gmail.com>
2025-03-27 23:23:03 +0100

    Added tests for createFinancialGoal and createFinancialGoal method

--- src/test/java/com/montelzek/moneytrack/service/FinancialGoalServiceImplTest.java
+++ src/test/java/com/montelzek/moneytrack/service/FinancialGoalServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.montelzek.moneytrack.service;
 
+import com.montelzek.moneytrack.dto.AccountDTO;
 import com.montelzek.moneytrack.dto.FinancialGoalDTO;
+import com.montelzek.moneytrack.model.Account;
 import com.montelzek.moneytrack.model.FinancialGoal;
 import com.montelzek.moneytrack.model.User;
 import com.montelzek.moneytrack.repository.FinancialGoalRepository;
@@ -17,6 +19,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -179,4 +182,104 @@ public class FinancialGoalServiceImplTest {
         assertThat(financialGoals).isEmpty();
         verify(financialGoalRepository).findTop3ByUserId(user.getId());
     }
+
+    @Test
+    public void saveFinancialGoal_idIsNull_shouldCreateNewFinancialGoal() {
+        // Arrange
+        FinancialGoalDTO newFinancialGoalDTO = FinancialGoalDTO.builder()
+                .id(null)
+                .name("New Financial Goal")
+                .targetAmount(BigDecimal.valueOf(1000))
+                .build();
+
+        when(userService.getCurrentUserId()).thenReturn(1L);
+        when(userService.findById(1L)).thenReturn(Optional.of(user));
+        when(financialGoalRepository.save(any(FinancialGoal.class))).thenReturn(financialGoal);
+
+        // Act
+        financialGoalService.saveFinancialGoal(newFinancialGoalDTO);
+
+        // Assert
+        verify(userService).getCurrentUserId();
+        verify(userService).findById(1L);
+        verify(financialGoalRepository).save(any(FinancialGoal.class));
+    }
+
+    @Test
+    public void saveFinancialGoal_idIsNotNull_shouldUpdateFinancialGoal() {
+        // Arrange
+        FinancialGoalDTO existingFinancialGoalDTO = FinancialGoalDTO.builder()
+                .id(1L)
+                .name("Update Financial Goal")
+                .targetAmount(BigDecimal.valueOf(2000))
+                .build();
+
+        when(financialGoalRepository.findById(1L)).thenReturn(Optional.of(financialGoal));
+        when(financialGoalRepository.save(financialGoal)).thenReturn(financialGoal);
+
+        // Act
+        financialGoalService.saveFinancialGoal(existingFinancialGoalDTO);
+
+        // Assert
+        assertThat(financialGoal.getName()).isEqualTo("Update Financial Goal");
+        verify(financialGoalRepository).findById(1L);
+        verify(financialGoalRepository).save(financialGoal);
+    }
+
+    @Test
+    public void createFinancialGoal_ShouldCreateNewFinancialGoal() {
+        // Arrange
+        when(userService.getCurrentUserId()).thenReturn(1L);
+        when(userService.findById(1L)).thenReturn(Optional.of(user));
+        when(financialGoalRepository.save(any(FinancialGoal.class))).thenReturn(financialGoal);
+
+        // Act
+        financialGoalService.createFinancialGoal(financialGoalDTO);
+
+        // Assert
+        verify(userService).getCurrentUserId();
+        verify(userService).findById(1L);
+        verify(financialGoalRepository).save(any(FinancialGoal.class));
+    }
+
+    @Test
+    public void createFinancialGoal_userNotFound_shouldThrowException() {
+        // Arrange
+        when(userService.getCurrentUserId()).thenReturn(1L);
+        when(userService.findById(1L)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThatExceptionOfType(IllegalStateException.class)
+                .isThrownBy(() -> financialGoalService.createFinancialGoal(financialGoalDTO))
+                .withMessage("User not found");
+    }
+
+    @Test
+    public void updateFinancialGoal_shouldUpdateExistingFinancialGoal() {
+        // Arrange
+        when(financialGoalRepository.findById(1L)).thenReturn(Optional.of(financialGoal));
+        when(financialGoalRepository.save(financialGoal)).thenReturn(financialGoal);
+
+        financialGoalDTO.setName("Updated Financial Goal Name");
+
+        // Act
+        financialGoalService.updateFinancialGoal(financialGoalDTO);
+
+        // Assert
+        assertThat(financialGoal.getName()).isEqualTo("Updated Financial Goal Name");
+        verify(financialGoalRepository).save(financialGoal);
+    }
+
+    @Test
+    public void updateFinancialGoal_idIsNull_shouldThrowException() {
+        // Arrange
+        financialGoalDTO.setId(null);
+
+        // Act & Assert
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> financialGoalService.updateFinancialGoal(financialGoalDTO))
+                .withMessage("Budget ID can't be null");
+    }
+
+
 }
