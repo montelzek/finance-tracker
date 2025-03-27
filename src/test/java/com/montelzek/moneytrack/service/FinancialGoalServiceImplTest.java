22980942ac87a12f0b26cbb6bc9b475c8c4ea270
montelzek <patrykmatras378@gmail.com>
2025-03-27 20:46:07 +0100

    Added tests for save, findById, and deleteById methods

--- src/test/java/com/montelzek/moneytrack/service/FinancialGoalServiceImplTest.java
+++ src/test/java/com/montelzek/moneytrack/service/FinancialGoalServiceImplTest.java
@@ -13,8 +13,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -81,5 +83,59 @@ public class FinancialGoalServiceImplTest {
         assertThat(result).isNotNull();
         assertThat(result).isEmpty();
         verify(financialGoalRepository).findByUserId_OrderByCreatedAt(-1L);
+    }
+
+    @Test
+    public void save_validFinancialGoal_shouldSaveFinancialGoal() {
+        // Arrange
+        when(financialGoalRepository.save(financialGoal)).thenReturn(financialGoal);
+
+        // Act
+        FinancialGoal savedFinancialGoal = financialGoalService.save(financialGoal);
+
+        // Assert
+        assertThat(savedFinancialGoal).isEqualTo(financialGoal);
+        verify(financialGoalRepository).save(financialGoal);
+    }
+
+    @Test
+    public void findById_existingId_shouldReturnFinancialGoal() {
+        // Arrange
+        Long financialGoalId = financialGoal.getId();
+        when(financialGoalRepository.findById(financialGoalId)).thenReturn(Optional.of(financialGoal));
+
+        // Act
+        FinancialGoal foundFinancialGoal = financialGoalService.findById(financialGoalId);
+
+        // Assert
+        assertThat(foundFinancialGoal).isNotNull();
+        assertThat(foundFinancialGoal.getName()).isEqualTo(financialGoal.getName());
+        assertThat(foundFinancialGoal.getTargetAmount()).isEqualTo(financialGoal.getTargetAmount());
+        assertThat(foundFinancialGoal.getCurrentAmount()).isEqualTo(financialGoal.getCurrentAmount());
+        assertThat(foundFinancialGoal.getIsAchieved()).isEqualTo(financialGoal.getIsAchieved());
+        verify(financialGoalRepository).findById(financialGoalId);
+    }
+
+    @Test
+    public void findById_nonExistingId_shouldThrowRuntimeException() {
+        // Arrange
+        Long nonExistingId = -1L;
+        when(financialGoalRepository.findById(nonExistingId)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> financialGoalService.findById(nonExistingId))
+                .withMessage("Financial Goal not find with id: " + nonExistingId);
+        verify(financialGoalRepository).findById(nonExistingId);
+    }
+
+    @Test
+    public void deleteById_shouldCallRepositoryDelete() {
+        // Arrange
+        Long testId = 1L;
+        // Act
+        financialGoalService.deleteById(testId);
+        //Assert
+        verify(financialGoalRepository).deleteById(testId);
     }
 }
