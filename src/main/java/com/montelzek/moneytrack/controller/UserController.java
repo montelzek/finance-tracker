bc2cda7910cb310ca9c08bbdf41fbe77262a8d4a
montelzek <patrykmatras378@gmail.com>
2025-04-06 20:49:01 +0200

    feat: Implement admin user management controller actions

--- src/main/java/com/montelzek/moneytrack/controller/UserController.java
+++ src/main/java/com/montelzek/moneytrack/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.montelzek.moneytrack.controller;
 
+import com.montelzek.moneytrack.model.Role;
 import com.montelzek.moneytrack.model.User;
 import com.montelzek.moneytrack.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -24,6 +26,7 @@ public class UserController {
     public String listUsers(Model model) {
         List<User> users = userService.findAll();
         model.addAttribute("users", users);
+        model.addAttribute("ERole", Role.ERole.class);
         return "admin-panel";
     }
 
@@ -33,5 +36,15 @@ public class UserController {
         return "redirect:/admin-panel";
     }
 
+    @PostMapping("/user/grant-premium")
+    public String grantPremium(@RequestParam("userId") Long id) {
+        userService.grantPremiumRole(id);
+        return "redirect:/admin-panel";
+    }
 
+    @PostMapping("/user/revoke-premium")
+    public String revokePremium(@RequestParam("userId") Long id) {
+        userService.revokePremiumRole(id);
+        return "redirect:/admin-panel";
+    }
 }
